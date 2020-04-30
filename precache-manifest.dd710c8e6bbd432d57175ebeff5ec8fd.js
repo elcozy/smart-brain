b22f1c2e4e664aeaf38186564bf6433e7db8a51e
Reynolds M <elcozy@users.noreply.github.com>
2020-05-01 00:43:47 +0100

    Updates

---
+++ precache-manifest.dd710c8e6bbd432d57175ebeff5ec8fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba3403100a22f7bdd020e36b4f701c1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca543be935e6f4710e27",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "fd696d107b17118c8c4b",
+    "url": "/static/css/main.cd384c93.chunk.css"
+  },
+  {
+    "revision": "ca543be935e6f4710e27",
+    "url": "/static/js/2.a161ce27.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.a161ce27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd696d107b17118c8c4b",
+    "url": "/static/js/main.ab1f9156.chunk.js"
+  },
+  {
+    "revision": "024c6d9d0a197a1cbf3b",
+    "url": "/static/js/runtime-main.970325b7.js"
+  }
+]);
